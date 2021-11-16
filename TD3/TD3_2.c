8f263fb9828ed593fec9b25bd1438bb6dffa8615
RizlaneAbalil <38002332@parisnanterre.fr>
2021-11-16 17:36:46 +0100

    TD3

--- TD3/TD3_2.c
+++ TD3/TD3_2.c
@@ -4,13 +4,23 @@
 #include<stdlib.h>
 #include<semaphore.h>
 
+sem_t semaphore1, semaphore2; 
 int Val; 
+pthread_mutex_t monMutex = PTHREAD_MUTEX_INITIALIZER; 
 
 void *Inc()
 {
+     
      for(int i=0; i<10000; i++
      {
+             sem_wait(&semaphore1);
              Val++; 
+             sem_post(&semaphore1);
+             /*
+             p_thread_mutex_lock(&monMutex)
+             Val++
+             p_thread_mutex_unlock(&monMutex)
+             */
 }
 
 }
@@ -19,7 +29,14 @@ void *Dec()
 {
      for(int i=0; i<10000; i++
      {
+             sem_wait(&semaphore2);
              Val--; 
+             sem_post(&semaphore2);
+             /*
+             p_thread_mutex_lock(&monMutex)
+             Val--
+             p_thread_mutex_unlock(&monMutex)
+             */
 }
 }
 
@@ -27,16 +44,13 @@ void main(void)
 {
      Val = 0; 
      pthread_t tacheInc, tacheDec; 
-     sem_t semInc, semDec; 
+     int sem1 = sem_init(&semaphore1, 0, 0); 
+     int sem2 = sem_init(&semaphore2, 0, 0); 
      
-     sem_wait(&semInc);
-     pthread_create(&tacheInc, NULL, Inc, NULL);
+     sem1 = pthread_create(&tacheInc, NULL, Inc, NULL);
      //pthread_join(tacheInc, NULL); 
-     sem_post(&semInc);
      
-     sem_wait(&semDec);
-     pthread_create(&tacheDec, NULL, Dec, NULL);
-     sem_post(&semDec);
+     sem2 = pthread_create(&tacheDec, NULL, Dec, NULL);
      
      pthread_join(tacheInc, NULL); 
      pthread_join(tacheDe, NULL); 
